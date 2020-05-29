3eca80cb56114c7d576025786c3299cffdc1eaf8
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2020-05-28 20:59:41 -0400

    Add files via upload

---
+++ foods.py
@@ -0,0 +1,10 @@
+copying lists
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:] # list with nothing in it just the colons replicates that list 
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
