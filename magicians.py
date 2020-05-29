271c0ae20942f26e9c07b534f054996ebebbb1e6
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2020-05-28 20:23:33 -0400

    Add files via upload

---
+++ magicians.py
@@ -0,0 +1,11 @@
+magicians = ['alice', 'david','carolina' ]
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick")
+
+sports = ['basketball', 'soccer', 'football', 'tennis', 'hockey']
+for sport in sports:
+    print(f"{sport.title()}, that is my favorite sport")
+
+celebs = ['Rihanna', 'Jlo', 'Beyonce', 'JayZ']
+for celeb in celebs:
+    print(f"{celeb.title()}, I love thier music")
