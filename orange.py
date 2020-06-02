f0d8d08fcd126b9f3f7490c767aa0a649c88bcfa
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2020-06-01 23:14:00 -0400

    Add files via upload

---
+++ orange.py
@@ -0,0 +1,10 @@
+class Orange:
+    def __init__(self, w, c):
+        self.weight = w
+        self.color = c
+        print("Created")
+
+
+orange_one = Orange(2, "orange")
+orange_two = Orange(9, "light orange")
+orange_three = Orange(7, "dark orange")
