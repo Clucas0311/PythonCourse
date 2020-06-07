c293e1832dbf5a4a6ba12609f08f8fec9e3af276
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2020-06-06 21:21:59 -0400

    Add files via upload

---
+++ some_condtion.py
@@ -0,0 +1,8 @@
+# What the if statement does it gives the code to go in one direction or another
+
+some_condition = True
+
+if some_condition:  # We don't have to put "is True" because the default condition is true
+    print("The variable  'some condition' is True ")
+else:  # if condition is not true check for something else
+    print("The variable 'some condition' is False")
