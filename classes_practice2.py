f3ec19665387648b9a3c9fc494d5846ec7e3f8c7
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2020-06-12 18:58:42 -0400

    Add files via upload

---
+++ classes_practice2.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self):
+        print("A new user has been made")
+
+
+user1 = User()  # instances - will print
+user2 = User()  # instances  - will print
+user3 = User()  # intances  - will print
